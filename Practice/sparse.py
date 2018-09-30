5d81d625f3be5893abd1de222110bc0bd2245691
chuang <ychin.chuang@gmail.com>
2018-09-30 12:58:29 +0800

    add sparse

--- Practice/sparse.py
+++ Practice/sparse.py
@@ -1,10 +1,9 @@
 from scipy import sparse
 help(sparse)			# check the functions of module sparse
 
-"""
-coo_matrix : use row, column, and value to create a matrix, however it cannot insert and delete elements
 
-"""
+# coo_matrix : use row, column, and value to create a matrix, however it cannot insert and delete elements
+
 row = [2, 2, 3]
 col = [3, 4, 2]
 data = [8, 9, 10]
@@ -12,10 +11,9 @@ c = sparse.coo_matrix((data, (row,col)), shape=(5,5))
 
 print(c.toarray())
 
-"""
-dok_matrix : use dictionary to store the element which isn't zero, add the element one by one
 
-"""
+# dok_matrix : use dictionary to store the element which isn't zero, add the element one by one
+
 d = sparse.dok_matrix((5,5), dtype=np.int16)
 for i in range(5):
     for j in range(5):
